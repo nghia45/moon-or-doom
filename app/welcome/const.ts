dadd72b90ce5be3fa8e3c9af78c8cf7f05804e93
NGHIANGUYEN7 <nghianguyen7@OMC-NGHIANGUYEN7-M.local>
2024-12-27 15:06:55 +0700

    initial commit

---
+++ app/welcome/const.ts
@@ -0,0 +1,9 @@
+export const GRID_LINE_COLOR = "rgba(255, 255, 255, 0.08)";
+export const LINE_COLOR = "rgba(204, 253, 7, 1)";
+export const AXIS_TEXT_COLOR = "#9da4ae";
+export const PLOT_LINE_COLOR = "#fd853a";
+
+export const INTERVAL = {
+    NORMAL: 1000,
+    SMOOTH: 250,
+}
